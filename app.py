974019748e0ea27efc518e1ef5bcb690bad2fcb0
Michael Rosenbaum <michaelr526@gmail.com>
2025-03-28 16:07:24 -0500

    Working fake data endpoints for Flask

--- app.py
+++ app.py
@@ -1,4 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+import json
 
 app = Flask(__name__)
 application = app
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        return redirect(url_for("local"))
+    return render_template("index.html")
+
+
+@app.route("/local", methods=["GET"])
+def local():
+    return render_template("local.html")
+
+
+@app.route("/response", methods=["GET"])
+def response():
+    # Page to return local information
+    data = {
+        "message": "Form submitted successfully",
+        "status": "200",
+        "data": [
+            {
+                "state": 17,
+                "fips": 17031,
+                "zip": 60615,
+                "obligation": "$1,000 ",
+                "paid": "$1,000 ",
+                "cut": "$10 ",
+                "desc": "Medicaid",
+            },
+            {
+                "state": 17,
+                "fips": 17031,
+                "zip": 60615,
+                "obligation": "$100 ",
+                "paid": "$90 ",
+                "cut": "$0 ",
+                "desc": "SNAP",
+            },
+        ],
+    }
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from fed-freeze-hack!")
-
-
-if __name__ == "__main__":
-    main()
