6b926d07beed977ce37c1699444a482c53ff8b6c
pravchand <praveenc@uchicago.edu>
2025-03-31 14:07:36 -0500

    added sqlite3 db

--- scripts/doge_api_script.py
+++ scripts/doge_api_script.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import csv
+import sqlite3
 
 URL_BASE = "https://api.doge.gov/savings/"
 HEADERS = {
@@ -10,14 +11,55 @@ HEADERS = {
 
 PER_PAGE = 500
 
-
+CONTRACT_SCHEMA = """
+CREATE TABLE IF NOT EXISTS contracts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    piid TEXT,
+    agency TEXT,
+    vendor TEXT,
+    value REAL,
+    description TEXT,
+    fpds_status TEXT,
+    fpds_link TEXT,
+    deleted_date TEXT,
+    savings REAL
+)
+"""
+GRANT_SCHEMA = """
+CREATE TABLE IF NOT EXISTS grants (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT,
+    agency TEXT,
+    recipient TEXT,
+    value REAL,
+    savings REAL,
+    link TEXT,
+    description TEXT
+)
+"""
+LEASE_SCHEMA = """
+CREATE TABLE IF NOT EXISTS leases (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT,
+    location TEXT,
+    sq_ft INTEGER,
+    description TEXT,
+    value REAL,
+    savings REAL,
+    agency TEXT
+)
+"""
 
 class SpendingItem:
     
     def __init__(self, name):
         self.name = name
         self.url = URL_BASE + self.name
-        
+
+    # Right now this returns all the items
+    # In a future iteration, we should only fetch the items that have been updated
+    # We can also return batches instead of all the items at once
+
     def fetch_all_items(self):
         all_data = []
         more_pages = True
@@ -57,15 +99,16 @@ class SpendingItem:
                 print(f"Error: API request failed with status code {response.status_code}")
                 print(response. text)
                 more_pages = False
-        
-        return all_data
+
+        all_data_tuples = [tuple(item.values()) for item in all_data]
+
+        return all_data_tuples
 
     def save_to_csv(self, items):
         filename= "./data/" + self.name + "_all.csv"
         if not self.name:
             print("No data to save to CSV")
             return
-        
     
         fieldnames = list(items[0].keys())
         
@@ -77,15 +120,62 @@ class SpendingItem:
         print(f"Data saved to {filename}")
 
 
+class DatabaseUpdater:
+    def __init__(self, name):
+        self.db_name = name
+        self.conn = sqlite3.connect(f'{self.db_name}.db')
+        self.cursor = self.conn.cursor()
+
+    def create_table(self):
+        self.cursor.execute(CONTRACT_SCHEMA)
+        self.cursor.execute(GRANT_SCHEMA)
+        self.cursor.execute(LEASE_SCHEMA)
+        self.conn.commit()
+
+    def save_to_db(self, batch_data, table_name):   
+        try:
+            if table_name == 'contracts':
+                self.cursor.executemany('''
+                INSERT INTO contracts (piid, agency, vendor, value, description, fpds_status, fpds_link, deleted_date, savings)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ''', batch_data)
+            elif table_name == 'grants':
+                self.cursor.executemany('''
+                INSERT INTO grants (date, agency, recipient, value, savings, link, description)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                ''', batch_data)        
+            elif table_name == 'leases':
+                self.cursor.executemany('''
+                INSERT INTO leases (date, location, sq_ft, description, value, savings, agency)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                ''', batch_data)
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print(f"Error saving data to {table_name}: {e}")
+    
+    def close_connection(self):
+        self.conn.close()
+
+
+
+
+
+db = DatabaseUpdater('./data/database/doge_api')
+db.create_table()
 
 contractsObject = SpendingItem('contracts')
 all_items = contractsObject.fetch_all_items()
-contractsObject.save_to_csv(all_items)
+# contractsObject.save_to_csv(all_items)
+db.save_to_db(all_items, 'contracts')
 
 grantsObject = SpendingItem('grants')
 all_items = grantsObject.fetch_all_items()
-grantsObject.save_to_csv(all_items)
+# grantsObject.save_to_csv(all_items)
+db.save_to_db(all_items, 'grants')
 
 leasesObject = SpendingItem('leases')
 all_items = leasesObject.fetch_all_items()
-leasesObject.save_to_csv(all_items)
+# leasesObject.save_to_csv(all_items)
+db.save_to_db(all_items, 'leases')
+
+db.close_connection()
